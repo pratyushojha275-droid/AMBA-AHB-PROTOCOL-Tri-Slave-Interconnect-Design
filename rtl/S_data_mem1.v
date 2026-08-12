82453d67865b26652ffb1808316cdc47eb472238
Pratyush Ojha <pratyushojha275@gmail.com>
2026-08-12 16:18:00 +0530

    Update S_data_mem1.v

--- rtl/S_data_mem1.v
+++ rtl/S_data_mem1.v
@@ -16,24 +16,6 @@ module S_data_mem1(
     reg [31:0]register[32:63];
     integer i;
     
-    initial begin
-    register[32] = 32'd90;
-    register[33] = 32'd91;
-    register[34] = 32'd92;
-    register[35] = 32'd93;
-    register[36] = 32'd94;
-    register[37] = 32'd95;
-    register[38] = 32'd96;
-    register[39] = 32'd97;
-    register[40] = 32'd98;
-    register[41] = 32'd99;
-    register[42] = 32'd100;
-    register[43] = 32'd101;
-    register[44] = 32'd102;
-    register[45] = 32'd104;
-    register[46] = 32'd105;
-    register[47] = 32'd107;
-    end
     reg [31:0]r3;
     reg prev_write;
     
